654e48db5f13a2206eda4d3a4aad1a2a25daa619
supriya <supriya.2896@gmail.com>
2017-06-20 12:20:23 +0530

    author test

---
+++ prjct/js/peasy.js
@@ -0,0 +1,6 @@
+var pmod=angular.module('peasy',[]);
+pmod.controller('pctrl',['$scope',function($scope)
+                        {
+                            $scope.message="wlcme";
+                            
+                        }]);
